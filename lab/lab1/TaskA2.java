7b5b2f580ecf42f0df9d071876fc9d084201bd47
Alexander Magdibur <dezzzert.official@gmail.com>
2025-11-29 04:02:00 +0300

    Add files via upload

---
+++ lab/lab1/TaskA2.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class TaskA2 {
+
+    // переводим буквы в числа
+    private static int getRomanValue(char romanChar) {
+        return switch (romanChar) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> throw new IllegalArgumentException("Недопустимый символ");
+        };
+    }
+
+    // составляем арабское число
+    public static int convertRoman(String romanNumber) {
+        if (romanNumber == null || romanNumber.trim().isEmpty()) {
+            throw new IllegalArgumentException("Пустая строка");
+        }
+
+        String roman = romanNumber.toUpperCase().trim();
+        int result = 0;
+        int prevValue = 0;
+
+        // идем справа налево и складываем числа
+        for (int i = roman.length() - 1; i >= 0; i--) {
+            char currentChar = roman.charAt(i);
+            int currentValue = getRomanValue(currentChar);
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        if (result <= 0) {
+            throw new IllegalArgumentException("Некорректное римское число");
+        }
+        return result;
+    }
+
+    // проверяем
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String roman = sc.nextLine();
+        System.out.println(convertRoman(roman));
+    }
+
+}

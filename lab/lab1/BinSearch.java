24f0681aa94fdbbf9182cfdad5539a4a94784764
Alexander Magdibur <dezzzert.official@gmail.com>
2025-11-29 14:43:10 +0300

    Update BinSearch.java

--- lab/lab1/BinSearch.java
+++ lab/lab1/BinSearch.java
@@ -1,66 +1,96 @@
+// Реализовать алгоритм бинарного поиска двумя способами.
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class b1 {
+public class BinSearch {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // ввод массива
+        int[] data = inputArray();
+
+        // сортировка
+        bubbleSort(data);
+        System.out.println("Отсортированный массив: " + Arrays.toString(data));
+
+        // число, которые нужно найти
+        System.out.print("Введите число: ");
+        int target = scanner.nextInt();
+
+        // итератиыный бин. поиск
+        int iterativeResult = iterativeBinarySearch(data, target);
+        System.out.println("Итеративный способ: " +
+                (iterativeResult != -1 ? "позиция " + iterativeResult : "элемент не найден"));
+
+        // рекурсивный бин. поиск
+        int recursiveResult = recursiveBinarySearch(data, target, 0, data.length - 1);
+        System.out.println("Рекурсивный способ: " +
+                (recursiveResult != -1 ? "позиция " + recursiveResult : "элемент не найден"));
+    }
+
+    // ввод массива
+    public static int[] inputArray() {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Введите размер массива: ");
-        int size = scanner.nextInt();
-        int[] nums = new int[size];
+        int n = scanner.nextInt();
 
-        System.out.println("Введите числа: ");
-        for (int i = 0; i < size; i++) {
-            nums[i] = scanner.nextInt();
-        }
-        int result = findNumber(nums);
-        if (result == -1) {
-            System.out.println("Числа не найдены");
-        } else {
-            System.out.println(result);
+        int[] arr = new int[n];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
         }
 
-        scanner.close();
+        return arr;
     }
 
-    public static int findMax(int[] arr) {
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-
-    public static int findNumber(int[] nums) {
-        if (nums.length == 0) {
-            return -1;
-        }
-
-        int maxNum = findMax(nums);
-
-        // массив для подсчета частот
-        int[] frequency = new int[maxNum + 1];
-
-        // подсчитываем частоты
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= 0 && nums[i] <= maxNum) {
-                frequency[nums[i]]++;
-            }
-        }
-
-        // ищем число, где значение равно частоте
-        int result = -1;
-        for (int i = 0; i < frequency.length; i++) {
-            if (i == frequency[i]) {
-                if (i > result) {
-                    result = i;
+    // сортировка пузырьком
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
+    }
 
-        return result;
+
+    // итеративный способ
+    public static int iterativeBinarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+
+    // рекурсивный способ
+    public static int recursiveBinarySearch(int[] arr, int target, int left, int right) {
+        if (left > right)
+            return -1;
+
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            return recursiveBinarySearch(arr, target, mid + 1, right);
+        else
+            return recursiveBinarySearch(arr, target, left, mid - 1);
     }
 
 }

d8393f56bde4fac1acd2e667bf022b3c0a2964da
Alexander Magdibur <dezzzert.official@gmail.com>
2025-11-29 14:40:56 +0300

    Update TaskA1.java

--- lab/lab1/TaskA1.java
+++ lab/lab1/TaskA1.java
@@ -3,37 +3,36 @@ import java.util.Scanner;
 
 public class TaskA1 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(convertNumber(n, m));
+    }
+
     // конвертируем число
-    public static String convertNumber(int N, int M) {
-        if (M < 2 || M > 9) {
+    public static String convertNumber(int n, int m) {
+        if (m < 2 || m > 9) {
             throw new IllegalArgumentException("Неверное основание системы счисления");
         }
-        if (N == 0) {
+        if (n == 0) {
             return "0";
         }
-        boolean isNegative = N < 0;
-        int number = Math.abs(N);
+        boolean isNegative = n < 0;
+        int number = Math.abs(n);
         StringBuilder result = new StringBuilder();
 
         // заполняем строку
         while (number > 0) {
-            int remainder = number % M;
+            int remainder = number % m;
             result.insert(0, remainder);
-            number = number / M;
+            number = number / m;
         }
         if (isNegative) {
             result.insert(0, '-');
         }
 
         return result.toString();
-    }
-
-    // проверяем
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    int M = sc.nextInt();
-    System.out.println(convertNumber(N, M));
     }
 
 }

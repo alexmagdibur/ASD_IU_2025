c4bf391f9a17bd7f4273e2425915f619a6c6d408
Alexander Magdibur <dezzzert.official@gmail.com>
2025-11-29 04:19:03 +0300

    Add files via upload

---
+++ lab/lab1/TaskB1.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class TaskB1 {
+
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static int findNumber(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+
+        int maxNum = findMax(nums);
+
+        // массив для подсчета частот
+        int[] frequency = new int[maxNum + 1];
+
+        // подсчитываем частоты
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0 && nums[i] <= maxNum) {
+                frequency[nums[i]]++;
+            }
+        }
+
+        // ищем число, где значение равно частоте
+        int result = -1;
+        for (int i = 0; i < frequency.length; i++) {
+            if (i > 0 && i == frequency[i]) {
+                if (i > result) {
+                    result = i;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+        int[] nums = new int[size];
+
+        System.out.println("Введите числа: ");
+        for (int i = 0; i < size; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        int result = findNumber(nums);
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
